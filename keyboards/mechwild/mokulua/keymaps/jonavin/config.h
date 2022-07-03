46d2f8864975072101729ad37358150e4b89fc5f
Jonavin <=>
2022-07-03 11:18:41 -0400

    Add new keymap files - incomplete

---
+++ keyboards/mechwild/mokulua/keymaps/jonavin/config.h
@@ -0,0 +1,19 @@
+// Copyright 2022 Kyle McCreery (@kylemccreery)
+// SPDX-License-Identifier: GPL-2.0-or-later
+// Copyright 2022 Jonavin Eng (@Jonavin)
+
+#pragma once
+
+#include "config_common.h"
+#define RGBLIGHT_LAYERS
+
+#define TAPPING_TOGGLE 2
+// TT set to two taps
+
+/*  Handle GRAVESC combo keys */
+#define GRAVE_ESC_ALT_OVERRIDE
+    //Always send Escape if Alt is pressed
+#define GRAVE_ESC_CTRL_OVERRIDE
+    //Always send Escape if Control is pressed
+
+#define TAPPING_TERM 180
